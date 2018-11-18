c4882c74a8164cc7794b38d2a07eb64b12143bfd
xuansonkrt <43500435+xuansonkrt@users.noreply.github.com>
2018-11-18 23:23:58 +0700

    mới nhất

--- count/count/frmMain.Designer.cs
+++ count/count/frmMain.Designer.cs
@@ -40,6 +40,8 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.btnTutorio = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -126,7 +128,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(255, 290);
+            this.label5.Location = new System.Drawing.Point(255, 335);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(343, 19);
             this.label5.TabIndex = 4;
@@ -136,37 +138,61 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(266, 312);
+            this.label6.Location = new System.Drawing.Point(266, 357);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(306, 19);
+            this.label6.Size = new System.Drawing.Size(387, 19);
             this.label6.TabIndex = 5;
-            this.label6.Text = "Sinh viên thực hiện: Nguyễn Quang Vĩnh";
+            this.label6.Text = "Sinh viên thực hiện: Nguyễn Quang Vĩnh - KTPM15";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(435, 334);
+            this.label7.Location = new System.Drawing.Point(435, 379);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(137, 19);
+            this.label7.Size = new System.Drawing.Size(213, 19);
             this.label7.TabIndex = 6;
-            this.label7.Text = "Nguyễn Xuân Sơn";
+            this.label7.Text = "Nguyễn Xuân Sơn - CNTT15";
             // 
             // btnTutorio
             // 
             this.btnTutorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTutorio.Location = new System.Drawing.Point(13, 399);
+            this.btnTutorio.Location = new System.Drawing.Point(12, 411);
             this.btnTutorio.Name = "btnTutorio";
             this.btnTutorio.Size = new System.Drawing.Size(120, 35);
             this.btnTutorio.TabIndex = 7;
             this.btnTutorio.Text = "Tutorio";
             this.btnTutorio.UseVisualStyleBackColor = true;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(72, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(672, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Đề tài: Viết chương trình count đếm số lần xuất hiện của một chuỗi trong các file" +
+    " text(ASCII)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(120, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(515, 17);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "được liệt kê trong dòng lệnh, không phân biệt chữ thường và chữ hoa.";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 446);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(820, 458);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.btnTutorio);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -201,5 +227,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btnTutorio;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
